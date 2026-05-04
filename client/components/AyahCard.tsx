21ba486998944f4a5861bb466152b7126cb78c1d
Maruf Hosen <maruf@thekowcompany.com>
2026-05-04 16:44:08 +0600

    audio update

--- client/components/AyahCard.tsx
+++ client/components/AyahCard.tsx
@@ -1,12 +1,51 @@
+// "use client";
+
+// import { useSettings } from "@/app/context/SettingContext";
+
+// export default function AyahCard({ ayah }: any) {
+//   const { arabicFont, arabicSize, translationSize } = useSettings();
+
+//   return (
+//     <div className="p-4 bg-[#11161c] rounded-xl space-y-2">
+//       {/* ARABIC */}
+//       <p
+//         className="text-right"
+//         style={{
+//           fontFamily: arabicFont,
+//           fontSize: arabicSize + "px",
+//         }}
+//       >
+//         {ayah.arabic}
+//       </p>
+
+//       {/* TRANSLATION */}
+//       <p
+//         style={{
+//           fontSize: translationSize + "px",
+//         }}
+//         className="text-gray-300"
+//       >
+//         {ayah.translation}
+//       </p>
+//     </div>
+//   );
+// }
+
 "use client";
 
 import { useSettings } from "@/app/context/SettingContext";
+import AudioPlayer from "@/components/AudioPlayer";
 
 export default function AyahCard({ ayah }: any) {
   const { arabicFont, arabicSize, translationSize } = useSettings();
 
   return (
-    <div className="p-4 bg-[#11161c] rounded-xl space-y-2">
+    <div className="p-4 bg-[#11161c] rounded-xl space-y-2 ">
+      {/* 🔊 AUDIO BUTTON */}
+      <div className="text-right">
+        <AudioPlayer number={ayah.globalNumber} />
+      </div>
+
       {/* ARABIC */}
       <p
         className="text-right"
